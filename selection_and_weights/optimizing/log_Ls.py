16bdb15f3c45dc63d11ea1745262e102ce7e3741
iclrsubmitter22 <iclr_submitter22@outlook.com>
2021-10-11 14:40:03 +0200

    new try

---
+++ selection_and_weights/optimizing/log_Ls.py
@@ -0,0 +1,31 @@
+import numpy as np 
+import os 
+import sys 
+from tqdm import tqdm
+
+allpathin = sys.argv[1]
+allpathout = sys.argv[2] 
+
+
+def folder_treat(pathin, pathout): 
+
+    matricesin = os.listdir(pathin) 
+    if not os.path.exists(pathout):
+        os.makedirs(pathout)
+
+
+    matrices = [os.path.join(pathin, x) for x in matricesin] 
+    for matrix in (matricesin) :
+        print(matrix)
+        values = np.load(os.path.join(pathin, matrix)) 
+        log = np.log(values) 
+        np.save(os.path.join(pathout, matrix), log)
+if not os.path.exists(allpathout):
+    os.makedirs(allpathout)
+for name in tqdm(os.listdir(allpathin)): 
+    pathin  = os.path.join(allpathin, name)
+    pathout=os.path.join(allpathout,name)
+    folder_treat(pathin, pathout)
+
+    
+
